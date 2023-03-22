c80b7ec0b616e16f265631291994f7f0be74c6f4
My Name <you@example.com>
2023-03-22 10:56:51 -0700

    Add JokeListView for jokes app in view.py file Add path to JokeListView in jokes URLConf file jokes\urls.py Add template joke_list.html in templates\jokes\joke_list.html Update link to Jokes in header of _base.html file

--- jokes/urls.py
+++ jokes/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 
+# import JokeListView from the jokes\views.py file in same directory with urls.py
+from .views import JokeListView
+
 # URLConf has a namespace of “jokes,” which is also the name of the app
 app_name = 'jokes'
-urlpatterns = []
+
+# add path to the JokeListVIew for jokes app
+urlpatterns = [
+  path('', JokeListView.as_view(), name='list'),
+]
--- jokes/views.py
+++ jokes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create JokeListView views for listing jokes in jokes app
+
+from django.views.generic import ListView
+from .models import Joke
+
+class JokeListView(ListView):
+    model = Joke
