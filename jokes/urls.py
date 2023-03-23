0f47d9c7fb881a5c6e0e4da66596bc1376f65258
My Name <you@example.com>
2023-03-23 10:24:06 -0700

    Add JokeCreateView and JokeUpdateView to jokes app views.py Add path of JokeCreateView and JokeUpdateView to jokes app URLConf urls.py file Create joke_form.html template\jokes for create and update views Update joke_list.html template to add New button and Update button

--- jokes/urls.py
+++ jokes/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 # import JokeListView from the jokes\views.py file in same directory with urls.py
-from .views import JokeListView, JokeDetailView
+from .views import JokeListView, JokeDetailView, JokeCreateView, JokeUpdateView
 
 # URLConf has a namespace of “jokes,” which is also the name of the app
 app_name = 'jokes'
@@ -13,4 +13,9 @@ urlpatterns = [
   # add path to the JokeDetailView that require pk of the joke
   # also import JokeDetailView from views.py above
   path('joke/<int:pk>/', JokeDetailView.as_view(), name='detail'),
+
+  # add path to the JokeCreateView and JokeUpdateView required pk
+  # after import them from .views
+  path('joke/<int:pk>/update/', JokeUpdateView.as_view(), name='update'),
+  path('joke/create/', JokeCreateView.as_view(), name='create'),
 ]
--- jokes/views.py
+++ jokes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create JokeListView view for listing jokes in jokes app
 
-from django.views.generic import DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from .models import Joke
 
 class JokeListView(ListView):
@@ -12,3 +12,14 @@ class JokeListView(ListView):
 # after add import DetailView above
 class JokeDetailView(DetailView):
     model = Joke
+
+# Create JokeCreateView for jokes app to add new joke
+class JokeCreateView(CreateView):
+    model = Joke
+    fields = ['question', 'answer']
+
+# Create JokeUpdateView for jokes app to update joke
+class JokeUpdateView(UpdateView):
+    model = Joke
+    fields = ['question', 'answer']
+
