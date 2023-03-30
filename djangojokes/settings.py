322eb841d86fdb4e673bcc0bd3c889818b65ba5d
My Name <you@example.com>
2023-03-30 11:01:46 -0700

    Add STATICFILES_DIRS list for static folder in settings.py Create new folder static ih the djangojokes.com project Create new folders images and css in new static folder Add image files to static\images Add main.css file to static\css Update _base.html template to load static files

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -126,6 +126,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+# Add STACTICFILES_DIR list of project static folder
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
