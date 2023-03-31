123d354d449278566499df60ed4e0b9f8125241d
My Name <you@example.com>
2023-03-31 13:41:19 -0700

    Create CustomUserAdmin in the admin.py file under the user app

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+CustomUser = get_user_model()
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    # Add option fields to built-in fields username, password and password2
+    # (confirm password) in the UserAdmin class
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Optional Fields', {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name'),
+        }),
+    )
