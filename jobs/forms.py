41c5ae7883475eb02e6c3cf15b814ecd11b5f76d
My Name <you@example.com>
2023-04-06 13:49:46 -0700

    Add django form widget to jobs\forms.py file to improve the Job Writer Applicaton

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,11 +1,22 @@
 # import forms to create class job application form 
 from django import forms
+from datetime import datetime
 
 class JobApplicationForm(forms.Form):
-    first_name = forms.CharField()
+    first_name = forms.CharField(
+        widget=forms.TextInput(attrs={'autofocus':True})
+    )
     last_name = forms.CharField()
     email = forms.EmailField()
-    website = forms.URLField(initial='https://', required=False)
+    website = forms.URLField(
+        required=False,
+        widget=forms.URLInput(
+            attrs={
+                'size': 50,
+                'placeholder': 'https://www.example.com',
+            }
+        )
+    )
     emp_types = (
         (None, '---Please Choose---'),
         ('full-time', 'Full-time'),
@@ -13,22 +24,46 @@ class JobApplicationForm(forms.Form):
         ('contract-work', 'Contract work')
     )
     employment_type = forms.ChoiceField(choices=emp_types)
+    YEARS = range(datetime.now().year, datetime.now().year+2)
     start_date = forms.DateField(
-        help_text='The earliest date you can start working.'
+        help_text='The earliest date you can start working.',
+        widget=forms.SelectDateWidget(
+                years=YEARS,
+        )
     )
     date_avail = (
-        ('mon', 'MON'),
-        ('tue', 'TUE'),
-        ('wed', 'WED'),
-        ('thu', 'THu'),
-        ('fri', 'FRI')
+        ('1', 'MON'),
+        ('2', 'TUE'),
+        ('3', 'WED'),
+        ('4', 'THu'),
+        ('5', 'FRI')
     )
-    available_date = forms.MultipleChoiceField(
+    
+    # change to TypedMutlipleChoiceField to convert string to int 
+    # with coerce option to send to server
+    # available_date = forms.MultipleChoiceField(
+    available_date = forms.TypedMultipleChoiceField(
         choices=date_avail,
-        help_text='Select all days that you can work.'
+        coerce=int,
+        help_text='Select all days that you can work.',
+         widget=forms.CheckboxSelectMultiple(
+            attrs={
+                'checked': True
+            }
+         )
     )
-    desired_hourly_wage = forms.DecimalField()
-    cover_letter = forms.CharField()
+    desired_hourly_wage = forms.DecimalField(
+        widget=forms.NumberInput(
+            attrs={
+                'min': '10.00',
+                'max': '100.00',
+                'step': '.25'
+            }
+        )
+    )
+    cover_letter = forms.CharField(
+        widget=forms.Textarea(attrs={'cols': '75', 'rows': '5'})
+    )
     confirmation = forms.BooleanField(
         label='I certify that the information I have provided is true.'
     )
