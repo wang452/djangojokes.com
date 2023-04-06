35b078408cf5250f7d08146b7fb1ca819a08f853
My Name <you@example.com>
2023-04-05 19:21:04 -0700

    Add more django form fields to Job Application form jobs\forms.py file Update send_email function to get sender and to email addresses in local_settings.py file

--- common/utils/email.py
+++ common/utils/email.py
@@ -6,6 +6,9 @@ from django.conf import settings
 
 def send_email(to, subject, content, sender='admin@example.com'):
     sg = sendgrid.SendGridAPIClient(settings.SENDGRID_API_KEY)
+    # override the sender and to values with values from local_settings.py file
+    sender = settings.SENDER
+    to = settings.RECEIVER
     mail = Mail(
         from_email=sender,
         to_emails=to,
--- jobs/forms.py
+++ jobs/forms.py
@@ -5,3 +5,30 @@ class JobApplicationForm(forms.Form):
     first_name = forms.CharField()
     last_name = forms.CharField()
     email = forms.EmailField()
+    website = forms.URLField(initial='https://', required=False)
+    emp_types = (
+        (None, '---Please Choose---'),
+        ('full-time', 'Full-time'),
+        ('part-time', 'Part-time'),
+        ('contract-work', 'Contract work')
+    )
+    employment_type = forms.ChoiceField(choices=emp_types)
+    start_date = forms.DateField(
+        help_text='The earliest date you can start working.'
+    )
+    date_avail = (
+        ('mon', 'MON'),
+        ('tue', 'TUE'),
+        ('wed', 'WED'),
+        ('thu', 'THu'),
+        ('fri', 'FRI')
+    )
+    available_date = forms.MultipleChoiceField(
+        choices=date_avail,
+        help_text='Select all days that you can work.'
+    )
+    desired_hourly_wage = forms.DecimalField()
+    cover_letter = forms.CharField()
+    confirmation = forms.BooleanField(
+        label='I certify that the information I have provided is true.'
+    )
