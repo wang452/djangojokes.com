f729b3f040acc95b0cc189f9248e6b42d5f0eb58
My Name <you@example.com>
2023-04-12 07:49:23 -0700

    updated forms.py with comments for repalcement JobApplicantForm with forms.ModelForm

--- jobs/forms.py
+++ jobs/forms.py
@@ -9,22 +9,83 @@ from django.core.validators import URLValidator
 # import model Applicant for the JobApplicationForm inner class Meta
 from .models import Applicant
 
-def validate_future_date(value):
-    if value < datetime.now().date():
-        raise ValidationError(
-            message=f'{value} is in the past.', code='past_date'
-        )
-
 # ensure value entered for required fields
 def validate_checked(value):
     if not value:
         raise ValidationError("Required.")
 
 
-# replace JobApplicationForm to inherit forms.ModelForm to use model Applicant
-# class JobApplicationForm(forms.Form):
-
 class JobApplicationForm(forms.ModelForm):
+
+    DAYS = (
+        (1, 'MON'),
+        (2, 'TUE'),
+        (3, 'WED'),
+        (4, 'THU'),
+        (5, 'FRI')
+    )
+
+    available_days = forms.TypedMultipleChoiceField(
+        choices=DAYS,
+        coerce=int,
+        help_text = 'Check all days that you can work.',
+        widget = forms.CheckboxSelectMultiple(
+            attrs = {'checked':True}
+        )
+    )
+
+    confirmation = forms.BooleanField(
+        label = 'I certify that the information I have provided is true.',
+        validators=[validate_checked]
+    )
+
+    class Meta:
+        model = Applicant
+        fields = (
+            'first_name', 'last_name', 'email', 'website', 'employment_type',
+            'start_date', 'available_days', 'desired_hourly_wage',
+            'cover_letter', 'confirmation', 'job')
+        widgets = {
+            'first_name': forms.TextInput(attrs={'autofocus': True}),
+            'website': forms.TextInput(
+                attrs = {'placeholder':'https://www.example.com'}
+            ),
+            'start_date': forms.SelectDateWidget(
+                attrs = {
+                    'style': 'width: 31%; display: inline-block; margin: 0 1%'
+                },
+                years = range(datetime.now().year, datetime.now().year+2)
+            ),
+            'desired_hourly_wage': forms.NumberInput(
+                attrs = {'min':'10.00', 'max':'100.00', 'step':'.25'}
+            ),
+            'cover_letter': forms.Textarea(attrs={'cols': '100', 'rows': '5'})
+        }
+        error_messages = {
+            'start_date': {
+                'past_date': 'Please enter a future date.'
+            }
+        }
+
+
+# replace JobApplicationForm to inherit forms.ModelForm to use model Applicant
+
+# move validate_future_date to the jobs\models.py where new Job and
+# Applicant models created
+"""
+def validate_future_date(value):
+    if value < datetime.now().date():
+        raise ValidationError(
+            message=f'{value} is in the past.', code='past_date'
+        )
+"""
+
+# replace JobApplicationFrom to inherit forms.ModelForm with model Applicant
+# All fields in this form are redefined in the Applicant model in models.py
+# use the inner class Meta in redefined JobApplicationForm above to modify
+# widgets attributes of the field in the form for HTML form control display 
+"""
+class JobApplicationForm(forms.Form):
     first_name = forms.CharField(
         widget=forms.TextInput(attrs={'autofocus':True})
     )
@@ -46,24 +107,24 @@ class JobApplicationForm(forms.ModelForm):
     )
     emp_types = (
         (None, '---Please Choose---'),
-        ('ft', 'Full-time'),
-        ('pt', 'Part-time'),
+        ('full-time', 'Full-time'),
+        ('part-time', 'Part-time'),
         ('contract', 'Contract work')
     )
     employment_type = forms.ChoiceField(choices=emp_types)
     YEARS = range(datetime.now().year, datetime.now().year+2)
 
-    """
-    start_date = forms.DateField(
-        help_text='The earliest date you can start working.',
-        widget=forms.SelectDateWidget(
-            years=YEARS,
-            attrs={'style': 'width: 31%; display: inline-block; margin: 0 1%'}
-        ),
-        validators=[validate_future_date],
-        error_messages={'past_date': 'Please enter a future date.'}
-    )
-    """
+    
+    # start_date = forms.DateField(
+    #   help_text='The earliest date you can start working.',
+    #    widget=forms.SelectDateWidget(
+    #        years=YEARS,
+    #        attrs={'style': 'width: 31%; display: inline-block; margin: 0 1%'}
+    #    ),
+    #    validators=[validate_future_date],
+    #    error_messages={'past_date': 'Please enter a future date.'}
+    #)
+    
 
     start_date = forms.DateField(
         help_text='The earliest date you can start working.',
@@ -114,33 +175,4 @@ class JobApplicationForm(forms.ModelForm):
         # add validator to ensure confirmation is checked
         validators=[validate_checked]
     )
-
-    # add inner class Meta to customize fields and message
-    class Meta:
-        model = Applicant
-        fields = (
-            'first_name', 'last_name', 'email', 'website', 'employment_type',
-            'start_date', 'available_days', 'desired_hourly_wage',
-            'cover_letter', 'confirmation', 'job')
-        widgets = {
-            'first_name': forms.TextInput(attrs={'autofocus': True}),
-            'website': forms.TextInput(
-                attrs = {'placeholder':'https://www.example.com'}
-            ),
-            'start_date': forms.SelectDateWidget(
-                attrs = {
-                    'style': 'width: 31%; display: inline-block; margin: 0 1%'
-                },
-                years = range(datetime.now().year, datetime.now().year+2)
-            ),
-            'desired_hourly_wage': forms.NumberInput(
-                attrs = {'min':'10.00', 'max':'100.00', 'step':'.25'}
-            ),
-            'cover_letter': forms.Textarea(attrs={'cols': '100', 'rows': '5'})
-        }
-        error_messages = {
-            'start_date': {
-                'past_date': 'Please enter a future date.'
-            }
-        }
-
+"""
